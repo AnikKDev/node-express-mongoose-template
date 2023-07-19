26c954fca05a45b3a5590616dd42f8fd4b7b1c61
anikdev2016 <anikdev2016@gmail.com>
2023-07-19 22:41:28 +0800

    added no route handler

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,33 @@
-import express, { Application, NextFunction, request, response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 const app: Application = express();
 import cors from "cors";
 import router from "./app/routes";
+import globalErrorHanlder from "./app/middlewares/globalErrorHandler";
+import httpStatus from "http-status";
 // middlewares ==> with .use()
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // application routes
 app.use("/api/v1/", router);
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+// global error
+app.use(globalErrorHanlder);
+
+// not found route
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "Not Found",
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: "API Not Found",
+      },
+    ],
+  });
+  next();
 });
 
 export default app;
